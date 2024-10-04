94db097a4d58304fce489e180c90592a8f6a4dea
zaki2406 <103506115+zaki2406@users.noreply.github.com>
2024-10-04 15:59:45 +0530

    Update serializers.py
    base264

--- financial/serializers.py
+++ financial/serializers.py
@@ -1,5 +1,26 @@
 # your_app/serializers.py
+#from rest_framework import serializers
+
+#class FinancialDataSerializer(serializers.Serializer):
+#    file = serializers.FileField()
+
+
+# your_app/serializers.py
 from rest_framework import serializers
+import base64
+from django.core.files.base import ContentFile
+
+class Base64PDFFileField(serializers.FileField):
+    def to_internal_value(self, data):
+        # Check if this is a base64 string
+        if isinstance(data, str) and data.startswith('data:application/pdf;base64,'):
+            # Strip the base64 header and decode the data
+            format, pdfstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            file_name = f'uploaded_file.{ext}'
+            data = ContentFile(base64.b64decode(pdfstr), name=file_name)
+        
+        return super(Base64PDFFileField, self).to_internal_value(data)
 
 class FinancialDataSerializer(serializers.Serializer):
-    file = serializers.FileField()
+    file = Base64PDFFileField()
